736f7e3563c40db2ddd7f9f82fe6ec1a7201a3d8
LuisaFerro <luisaferro9@gmail.com>
2023-07-22 13:52:27 -0300

    starting_cpp_studies

---
+++ 00. p1roject/scr/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\n hello world \n\n");
+
+    int favorite_num;
+
+    std::cout << "enter your favorite number between 1 and 100: ";
+
+    std::cin >> favorite_num;
+
+    std::cout << "Wow, ";
+    std:: cout << favorite_num;
+    printf(" is my favorite too \n\n");
+    
+
+    return 0;
+
+};
+
